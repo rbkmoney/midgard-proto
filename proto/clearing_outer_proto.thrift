833154a16fcab9b4e1481e78646e352162391d9e
d.baykov <d.baykov@rbkmoney.com>
2019-06-20 18:00:14 +0300

    Add remote clearing event status change [2]

--- proto/clearing_outer_proto.thrift
+++ proto/clearing_outer_proto.thrift
@@ -32,8 +32,6 @@ service ClearingService {
     void StartClearingEvent(1: ClearingEvent clearingEvent) throws (1: ProviderNotFound ex1)
     /** Получение статуса клирингового события */
     ClearingEventStateResponse GetClearingEventState(1: EventID event_id) throws (1: NoClearingEvent ex1)
-    /** Перевести клиринговое событие из статуса "Выполняется" в статус "Завершено" */
-    void CompleteClearingEvent(1: ClearingEvent clearingEvent) throws (1: NoClearingEvent ex1)
     /** Повторно отправить клиринговый файл */
     void ResendClearingFile(1: ClearingEvent clearingEvent) throws (1: NoClearingEvent ex1)
 }
